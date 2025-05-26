8d58d7c78870fa82ae5d143f50c6ab5276f0df3c
OHMll <napadechkaewnok@gmail.com>
2025-05-27 02:34:09 +0700

    [CREATE]สร้าง API register

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -13,6 +13,19 @@ const hashPasswordWithSalt = async (plainPassword: string) => {
 
 // const users: { id: number; username: string; password: string }[] = [];
 
+function generateDefaultAvatar(letter: any) {
+  const svg = `
+  <svg width="128" height="128" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="#4F46E5"/>
+    <text x="50%" y="50%" font-size="64" fill="white" dominant-baseline="middle" text-anchor="middle" font-family="Arial, sans-serif">
+      ${letter}
+    </text>
+  </svg>
+  `;
+  const base64 = Buffer.from(svg).toString("base64");
+  return `data:image/svg+xml;base64,${base64}`;
+}
+
 export const registerUser = async (req: Request, res: Response) => {
 
   const {
@@ -21,23 +34,20 @@ export const registerUser = async (req: Request, res: Response) => {
     user_first_name,
     user_last_name,
     sex,
+    phone,
+    weight,
+    height
   } = req.body;
 
   const passwordHasing = await hashPasswordWithSalt(password)
   const username = email.split('@')[0];
   const create_at = new Date().toISOString();
-
-  // เอา email ไปทำ OTP
-
-
-
-
-
-  // OTP section ถ้าผ่านทำต่อ ถ้าไม่ผ่าน return error
+  const firstChar = user_first_name.trim().charAt(0).toUpperCase();
+  const avatar_url = generateDefaultAvatar(firstChar);
 
   const query = `
-    INSERT INTO user_sys (username, email, password, user_first_name, user_last_name, sex, create_at)
-    VALUES ('${username}', '${email}', '${passwordHasing}' , '${user_first_name}' , '${user_last_name}', '${sex}', '${create_at}')
+    INSERT INTO user_sys (username, email, password, user_first_name, user_last_name, avatar_url, create_at, sex, phone, weight, height)
+    VALUES ('${username}', '${email}', '${passwordHasing}' , '${user_first_name}' , '${user_last_name}', '${avatar_url}', '${create_at}', '${sex}', '${phone}', ${weight}, ${height})
     RETURNING *;
   `;
 
@@ -46,10 +56,10 @@ export const registerUser = async (req: Request, res: Response) => {
   try {
     const data = await queryPostgresDB(query, globalSmartGISConfig);
     res.status(200).json({ success: true, data });
-} catch (error) {
+  } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ success: false, message: 'Error fetching data' });
-}
+  }
 
 };
 
